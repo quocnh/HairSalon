0d56179f6a23f77639e10b79d7f1713642b372ca
LarryNguyen <ntiendung@gmail.com>
2020-08-04 00:21:14 +0900

    Add based code for frontend
    Add based code for frontend

---
+++ frontend/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { AdminLayoutRoutes } from './admin-layout.routing';
+
+import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
+import { UserComponent } from '../../pages/user/user.component';
+import { TableComponent } from '../../pages/table/table.component';
+import { TypographyComponent } from '../../pages/typography/typography.component';
+import { IconsComponent } from '../../pages/icons/icons.component';
+import { MapsComponent } from '../../pages/maps/maps.component';
+import { NotificationsComponent } from '../../pages/notifications/notifications.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DemoPageComponent } from '../../pages/demo-page/demo-page.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(AdminLayoutRoutes),
+    FormsModule,
+    NgbModule
+  ],
+  declarations: [
+    DemoPageComponent,
+    DashboardComponent,
+    UserComponent,
+    TableComponent,
+    TypographyComponent,
+    IconsComponent,
+    MapsComponent,
+    NotificationsComponent
+  ]
+})
+
+export class AdminLayoutModule {}
---
+++ frontend/src/app/pages/demo-page/demo-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
+
+@Component({
+  selector: 'app-demo-page',
+  moduleId: module.id,
+  templateUrl: 'demo-page.component.html'
+})
+export class DemoPageComponent implements OnInit {
+
+
+  constructor() { }
+
+    ngOnInit() {
+    }
+
+
+}
