e44c45f30f67ceb9bc0dccbc5b7ea629eacbab27
LarryNguyen <ntiendung@gmail.com>
2020-08-25 01:35:18 +0900

    change customer DB struct

--- frontend/src/app/popup/add-new-customer/add-new-customer.component.ts
+++ frontend/src/app/popup/add-new-customer/add-new-customer.component.ts
@@ -9,11 +9,6 @@ import Customer from '../../module/customer';
 })
 export class AddNewCustomerComponent implements OnInit {
   public customer: Customer = new Customer();
-  firstName: string;
-  lastName: string;
-  city: string;
-  district: string;
-  address: string;
 
   constructor(
     public modal: NgbActiveModal,
@@ -24,13 +19,8 @@ export class AddNewCustomerComponent implements OnInit {
   }
 
   addNewCustomer() {
-    this.getCustomerInfo();
     console.log(this.customer);
     this.modal.close(this.customer);
-  }
-
-  getCustomerInfo() {
-    this.customer.fullname = this.firstName + ' ' + this.lastName;
   }
 
 }
