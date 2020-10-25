11ba38b82724bc21b40304b135eae897f79db2d0
Mark Nguyen <huuquoc09@gmail.com>
2020-10-25 11:46:39 -0500

    fix login bug

--- backend/app.js
+++ backend/app.js
@@ -31,8 +31,6 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(passport.initialize());
 
-require("");
-
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
