f13d39005c58d1df942dc1613b7d72c5d0664bef
Mark Nguyen <huuquoc09@gmail.com>
2021-01-11 12:25:08 -0600

    design page builder for spash screen

--- mobile/salonmobile/lib/screens/splash/components/body.dart
+++ mobile/salonmobile/lib/screens/splash/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:salonmobile/constants.dart';
 import 'package:salonmobile/size_config.dart';
 
 class Body extends StatefulWidget {
@@ -7,25 +8,81 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  List<Map<String, String>> splashData = [
+    {
+      "text": "Chào bạn đến với Awinst, thiên đường làm đẹp!",
+      "image": "assets/images/splash_1.png"
+    },
+    {
+      "text":
+          "Chúng tôi kết nối hàng ngàn cửa hàng salon \n trên khắp đất nước hình chữ S",
+      "image": "assets/images/splash_2.png"
+    },
+    {
+      "text":
+          "Làm đẹp và trải nghiệm tại Awinst tiện lợi hơn bao giờ hết. \nBạn chỉ cần ở nhà và tận hưởng!",
+      "image": "assets/images/splash_3.png"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Column(
-              children: <Widget>[
-                Text(
-                  "AWINST",
-                  style: TextStyle(fontSize: getProportionateScreenWidth(36)),
-                )
-              ],
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                itemCount: splashData.length,
+                itemBuilder: (context, index) => SplashContent(
+                  text: splashData[index]["text"],
+                  image: splashData[index]["image"],
+                ),
+              ),
             ),
-          ),
-          Expanded(flex: 2, child: SizedBox())
-        ],
+            Expanded(flex: 2, child: SizedBox())
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key key,
+    this.text,
+    this.image,
+  }) : super(key: key);
+  final String text, image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Spacer(),
+        Text(
+          "AWINST",
+          style: TextStyle(
+              fontSize: getProportionateScreenWidth(36),
+              color: kPrimaryColor,
+              fontWeight: FontWeight.bold),
+        ),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+        ),
+        Spacer(
+          flex: 2,
+        ),
+        Image.asset(
+          image,
+          height: getProportionateScreenHeight(265),
+          width: getProportionateScreenWidth(235),
+        )
+      ],
     );
   }
 }
