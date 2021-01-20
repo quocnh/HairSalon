982127bb22de29d180bd4dca7338cb3f67a2963b
Mark Nguyen <huuquoc09@gmail.com>
2021-01-20 08:51:05 -0600

    translate sign in sign up title bar

--- mobile/salonmobile/lib/screens/sign_up/sign_up_screen.dart
+++ mobile/salonmobile/lib/screens/sign_up/sign_up_screen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:salonmobile/utils/app_localizations.dart';
 
 class SignUpScreen extends StatelessWidget {
   static String routeName = "/sign_up";
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context).translate('sign_up')),
+      ),
+    );
   }
 }
