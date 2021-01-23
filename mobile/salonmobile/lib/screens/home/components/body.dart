04bcae8b972ef0ddd611f721e1a6b11bda32ca9e
Mark Nguyen <huuquoc09@gmail.com>
2021-01-23 01:03:19 -0600

    home

--- mobile/salonmobile/lib/screens/home/components/body.dart
+++ mobile/salonmobile/lib/screens/home/components/body.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:salonmobile/screens/home/components/categories.dart';
+import 'package:salonmobile/screens/home/components/discount_banner.dart';
+import 'package:salonmobile/screens/home/components/home_header.dart';
+import 'package:salonmobile/screens/home/components/popular_product.dart';
+import 'package:salonmobile/screens/home/components/special_offers.dart';
+import 'package:salonmobile/utils/size_config.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: getProportionateScreenHeight(20)),
+            HomeHeader(),
+            SizedBox(height: getProportionateScreenWidth(10)),
+            DiscountBanner(),
+            Categories(),
+            SpecialOffers(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            PopularProducts(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ mobile/salonmobile/lib/screens/home/components/home_header.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:salonmobile/screens/home/components/icon_btn_with_counter.dart';
+import 'package:salonmobile/screens/home/components/search_field.dart';
+import 'package:salonmobile/screens/splash/splash_screen.dart';
+import 'package:salonmobile/utils/size_config.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SearchField(),
+          IconBtnWithCounter(
+            svgSrc: "assets/icons/Cart Icon.svg",
+            press: () => Navigator.pushNamed(context, SplashScreen.routeName),
+            // press: () => Navigator.pushNamed(context, CartScreen.routeName),
+          ),
+          IconBtnWithCounter(
+            svgSrc: "assets/icons/Bell.svg",
+            numOfitem: 3,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
