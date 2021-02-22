af1a6feb56b3846802bfeca8d58bc9038918669a
Mark Nguyen <huuquoc09@gmail.com>
2021-02-21 18:19:51 -0600

    changed home screen from stateless to statefull

--- mobile/salonmobile/lib/screens/home/home_screen.dart
+++ mobile/salonmobile/lib/screens/home/home_screen.dart
@@ -2,8 +2,13 @@ import "package:flutter/material.dart";
 import 'package:salonmobile/screens/home/components/body.dart';
 import 'package:salonmobile/utils/size_config.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static String routeName = "/home";
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
