caf84d3a632bfb1d912c5b61ac1085f416abba50
LarryNguyen <ntiendung@gmail.com>
2021-03-02 22:52:12 +0900

    fix bug barber list

--- frontend/src/app/layouts/manager-layout/manager-layout.routing.ts
+++ frontend/src/app/layouts/manager-layout/manager-layout.routing.ts
@@ -28,6 +28,7 @@ export const ManagerLayoutRoutes: Routes = [
     { path: 'profile',                      component: UserProfileComponent },
     { path: 'salons',               component: SalonsListViewComponent },
     { path: 'salons/:salonId',               component: SalonEditComponent },
+    { path: 'salons/barbers/:salonId',               component: BarberViewComponent },
 
     { path: 'distributors',         component: DistributorListViewComponent },
     { path: 'distributors/:distributorId/products/:productId',         component: ProductDetailViewComponent },
