3c97837b7787276bfaef35126eff139c047d8376
Mark Nguyen <huuquoc09@gmail.com>
2021-06-16 20:53:01 -0500

    changed name of search salon menu

--- mobile/salonmobile/lib/screens/search_salons/list_store_screen.dart
+++ mobile/salonmobile/lib/screens/search_salons/list_store_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:salonmobile/screens/list_store/components/body.dart';
 
 
-class ListStoreScreen extends StatelessWidget {
+class SearchSalonsScreen extends StatelessWidget {
   static String routeName = "";
   @override
   Widget build(BuildContext context) {
