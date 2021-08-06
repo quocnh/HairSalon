47d9a40ef88b8cfb009b6dd8153c806f58a9f139
LarryNguyen <ntiendung@gmail.com>
2021-08-06 18:22:15 +0900

    fix for production

--- backend_new/app/config/db.config.js
+++ backend_new/app/config/db.config.js
@@ -4,8 +4,8 @@ module.exports = {
     DB: "hairsalon_db",
     MG_USERNAME: "hairsalon_admin",
     MG_PASSWORD: "hairsalon_admin",
-    //NODEJS_HOST: 'https://awinst.com:3000',
-    //WEB_HOST: 'https://awinst.com:4200',
-    NODEJS_HOST: 'https://localhost:3000',
-    WEB_HOST: 'https://localhost:4200',
+    NODEJS_HOST: 'https://awinst.com:3000',
+    WEB_HOST: 'https://awinst.com:4200',
+    //NODEJS_HOST: 'https://localhost:3000',
+    //WEB_HOST: 'https://localhost:4200',
   };
